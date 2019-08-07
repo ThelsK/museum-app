0add52c3d23dbf72746ef71f4faec9d239b6ce6c
ThelsK <github@thels.nl>
2019-08-07 11:08:34 +0200

    Created elements to hold the posted name and message

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,21 @@
 function submitComment() {
-    console.log("Hello!")
+
+    // Grab the name and message that were submitted.
+    const name = document.getElementById("name").value
+    const msg = document.getElementById("msg").value
+
+    // Create new elements to display the name and message.
+    const comment = document.createElement("section")
+    const h3 = document.createElement("h3")
+    const p = document.createElement("p")
+
+    // Apply the name and message to the new elements.
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+
+    // Add the new elements to the website.
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+    console.log(comment)
 }
